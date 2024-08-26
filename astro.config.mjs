258d037744a0bde56e70995ebac3e76de5adf9cc
Dhia' Shalabi <dhia.shalabi@gmail.com>
2024-08-26 22:46:45 +0300

    chore: Update Astro configuration and remove unused code

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     "site": "https://coworking-space.dhiashalabi.info",
-    base: 'coworking-space',
     integrations: [tailwind()],
 });

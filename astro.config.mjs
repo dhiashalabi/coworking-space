f0c4ec2b86754fb488e65b81e04cb8a0df1c2793
Dhia' Shalabi <dhia.shalabi@gmail.com>
2024-08-26 22:35:43 +0300

    feat: Add GitHub Pages deployment workflow and Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     "site": "https://coworking-space.dhiashalabi.info",
+    base: 'coworking-space',
     integrations: [tailwind()],
 });
